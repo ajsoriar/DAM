aa67f954c8effa2ba4c880deaa2f18252c0fe1a1
ajsoriar <ajsoriar@gmail.com>
2024-10-28 21:53:44 +0000

    Seome changes

--- DEV-OPS_P1_test_site/index.php
+++ DEV-OPS_P1_test_site/index.php
@@ -17,9 +17,9 @@
     <?php
 
         $host = 'localhost';
-        $db = 'CData MariaDB Sys';
-        $user = 'root';
-        $pass = '';
+        $db = 'mysql';
+        $user = 'newuser';
+        $pass = 'password';
 
         // URL-encode the database name
         $db_encoded = urlencode($db);
@@ -32,31 +32,22 @@
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
             // SQL query with backticks
-            $sql = "SELECT * FROM `CData MariaDB Sys`.`innodb_table_stats`";
+            $sql = "SELECT User, Host FROM user";
 
             $stmt = $pdo->prepare($sql);
             $stmt->execute();
 
-            echo "<h2>innodb_table_stats</h2>";
             echo "<table border='1'>";
             echo "<tr>
-                    <th>database_name</th>
-                    <th>table_name</th>
-                    <th>last_update</th>
-                    <th>n_rows</th>
-                    <th>clustered_index_size</th>
-                    <th>sum_of_other_index_sizes</th>
+                    <th>User</th>
+                    <th>Host</th>
                 </tr>";
 
             // Fetch data as an associative array
             while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
                 echo "<tr>
-                        <td>{$row['database_name']}</td>
-                        <td>{$row['table_name']}</td>
-                        <td>{$row['last_update']}</td>
-                        <td>{$row['n_rows']}</td>
-                        <td>{$row['clustered_index_size']}</td>
-                        <td>{$row['sum_of_other_index_sizes']}</td>
+                        <td>{$row['User']}</td>
+                        <td>{$row['Host']}</td>
                     </tr>";
             }
             echo "</table>";
